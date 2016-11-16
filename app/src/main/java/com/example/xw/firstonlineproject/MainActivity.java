5f1b4e6c4b649af9b4752d6c0d020cddcc7f1261
XiaoWangD <420028932@qq.com>
2016-11-16 10:13:07 +0800

    ”测试一下

--- app/src/main/java/com/example/xw/firstonlineproject/MainActivity.java
+++ app/src/main/java/com/example/xw/firstonlineproject/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * 这是一个测试
+     * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
